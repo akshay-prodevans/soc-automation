3b76017f5436c0502aee31db3ba022ee2d0bee5b
Akshay Harbhare <akshay@prodevans.com>
2018-01-24 11:48:38 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,4 +1,13 @@
-scp -r /opt/metron_files/localrepo root@192.168.1.235:/
-scp -r /opt/metron_files/localrepo root@192.168.1.207:/
-scp -r /opt/metron_files/localrepo root@192.168.1.79:/
-scp -r /opt/metron_files/localrepo root@192.168.1.189:/
+#!/bin/bash
+echo "Enter Ambari IP: "
+read ambari_ip
+echo "Enter Metron IP"
+read metron_ip
+echo "Enter Hadoop Node1 IP"
+read node1_ip
+echo "Enter Hadoop Node2 IP"
+read node2_ip
+scp -r /opt/metron_files/localrepo root@$ambari_ip:/
+scp -r /opt/metron_files/localrepo root@$metron_ip:/
+scp -r /opt/metron_files/localrepo root@$node1_ip:/
+scp -r /opt/metron_files/localrepo root@$node2_ip:/
